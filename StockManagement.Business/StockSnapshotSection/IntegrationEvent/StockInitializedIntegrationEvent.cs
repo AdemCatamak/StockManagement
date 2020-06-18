019887b831432a425e06ca9eb484c3193e1528b5
Adem Catamak <ademcatamak@gmail.com>
2020-06-18 13:35:06 +0300

    StockSnapshotCreatedIntegrationEvent is renamed as StockInitializedIntegrationEvent (#7)

--- StockManagement.Business/StockSnapshotSection/IntegrationEvent/StockInitializedIntegrationEvent.cs
+++ StockManagement.Business/StockSnapshotSection/IntegrationEvent/StockInitializedIntegrationEvent.cs
@@ -3,17 +3,15 @@ using StockManagement.Utility.IntegrationEventPublisherSection;
 
 namespace StockManagement.Business.StockSnapshotSection.IntegrationEvent
 {
-    public class StockSnapshotCreatedIntegrationEvent : IIntegrationEvent
+    public class StockInitializedIntegrationEvent : IIntegrationEvent
     {
         public long ProductId { get; }
-        public long StockSnapshotId { get; }
         public long StockActionId { get; }
         public DateTime StockCreatedOn { get; }
 
-        public StockSnapshotCreatedIntegrationEvent(long productId, long stockSnapshotId, long stockActionId, DateTime stockCreatedOn)
+        public StockInitializedIntegrationEvent(long productId, long stockActionId, DateTime stockCreatedOn)
         {
             ProductId = productId;
-            StockSnapshotId = stockSnapshotId;
             StockActionId = stockActionId;
             StockCreatedOn = stockCreatedOn;
         }
